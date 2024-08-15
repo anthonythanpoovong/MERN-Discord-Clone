6d75a5f167e20ec5da61cb045a6223442fe30fc6
Anthony Thanpoovong <thanpoovong84@gmail.com>
2024-08-15 17:40:50 -0400

    added firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,13 @@
-import firebase from 'firebase'
+import firebase from 'firebase';
 
 const firebaseConfig = {
-    // your firebase config
+    apiKey: "AIzaSyDwbfFpKj_zGdVEzJfbwQ9roN-tt8SeWRE",
+    authDomain: "discord-clone-cc885.firebaseapp.com",
+    projectId: "discord-clone-cc885",
+    storageBucket: "discord-clone-cc885.appspot.com",
+    messagingSenderId: "1043427732698",
+    appId: "1:1043427732698:web:e77474cb7d5b5cfa80c9e3",
+    measurementId: "G-ZFMC2FMNFH"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
@@ -10,5 +16,5 @@ const db = firebaseApp.firestore()
 const auth = firebase.auth()
 const provider = new firebase.auth.GoogleAuthProvider()
 
-export { auth, provider }
+export { auth, provider };
 export default db
